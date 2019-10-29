34c1ab904615f059f5601ef47b50b5fe9b525a23
schnei-nathan <schnei.nathan@gmail.com>
2019-10-29 02:13:01 -0400

    Python commented

--- linkedin_data_parser.py
+++ linkedin_data_parser.py
@@ -11,24 +11,31 @@ or run itself to parse all the members.
 from html.parser import HTMLParser
 import json
 
-#Global for easy access into the MyHTMLParser
+#Global list for easy access into the MyHTMLParser
 data_list = []
-#Path for html files
 
-DATA_PATH = "./member_jobs/"
+
+#Path for html files
+MEMBER_PATH = "./data/member_jobs/"
+JSON_PATH = "./data/DALI_Data.json"
+DESTINATION_PATH = "./data/job_data.json"
+
+
+#Variant on HTML parser for a simple data stripping of the page
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
-        if data.strip()!= "":
+        if data.strip()!= "": #strips data and adds them to the global list
             data_list.append(data.strip())
 
 
 #get_job_list takes a string of a name, and returns a list of dicts of their job experience
 def get_job_list(member_name):
 
-    parser = MyHTMLParser()
+    
+    parser = MyHTMLParser() #init the parser
 
     #Open their file with underscores in UTF-8
-    f = open("%s%s.html" %(DATA_PATH,member_name.replace(" ","_")),encoding="utf8")
+    f = open("%s%s.html" %(MEMBER_PATH,member_name.replace(" ","_")),encoding="utf8")
 
     #LinkedIn orders their pages in profile sections, this delimiter lets us focus on the relevant sections
     sections = f.read().split("pv-profile-section__card-heading")
@@ -47,6 +54,7 @@ def get_job_list(member_name):
     index = data_list.index("Company Name")
     #Add lists of data strings to the job list of lists
 
+    #Keywords to 
     keywords = ["duration","dates","location"]
 
     current_list_job.append(data_list[index-1])
@@ -79,7 +87,9 @@ def get_job_list(member_name):
         job_dict["extra"] = ""
         job_dict["location"]  = ""
         i = 0 
+
         #Check the current string for relevancy (comes in multiple orders)
+        #If there's a keyword, then add the next value to the job dictionary
         while i < len(current_list_job):
             if "company name" in current_list_job[i].lower():
                 job_dict["company_name"] = current_list_job[i+1]
@@ -89,7 +99,7 @@ def get_job_list(member_name):
                 job_dict["duration"] = current_list_job[i+1]
                 i+=1
             elif "location" in current_list_job[i].lower() \
-                and len(current_list_job[i].lower()) < 20:
+                and len(current_list_job[i].lower()) < 30:
                 job_dict["location"] = current_list_job[i+1]
                 i+=1
             elif "dates" in current_list_job[i].lower():
@@ -114,21 +124,23 @@ def get_job_list(member_name):
     data_list.clear()
     return job_dict_list
 
+#Main function runs the parser for all names it finds in the member directory
+def main():
+    #The file can be run by itself to load all the names and make a larger json
+    jfile = open(JSON_PATH)
+    member_list = json.loads(jfile.read())
+
+    member_id = 0
+    #Get their names and jobs from the saved files
+    for member in member_list:
+        name = member["name"]
+        member["id"] = member_id
+        member_id+=1
+        member["jobs"] = get_job_list(name)
 
 
-#The file can be run by itself to load all the names and make a larger json
-jfile = open("DALI_DATA.json")
-member_list = json.loads(jfile.read())
+    # Open and save the new job json
+    with open('./data/job_data.json', 'w') as outfile:
+        json.dump(member_list, outfile)
 
-member_id = 0
-#Get their names and jobs from the saved files
-for member in member_list:
-    name = member["name"]
-    member["id"] = member_id
-    member_id+=1
-    member["jobs"] = get_job_list(name)
-
-
-# Open and save the new job json
-with open('job_data.json', 'w') as outfile:
-    json.dump(member_list, outfile)
+main()
