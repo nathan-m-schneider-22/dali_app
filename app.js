26a390304d0e2e4c06711303a5f573b17fac05e2
schnei-nathan <schnei.nathan@gmail.com>
2019-10-28 21:15:47 -0400

    member field input validation

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const express = require('express')
+const flat = require('flat')
 const bodyParser = require('body-parser')
-
+var validate = require('express-validation')
 const app = express()
 const port = 3000
+var Joi = require('joi')
+var data = require('./job_data.json'); 
 
-var data = require('./job_data.json'); //(with path)
+var {member_schema,job_schema} = require('./schemas.js')
+
 app.use(bodyParser.json())
+
+  
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to the API\nSee the documentation for its uses")
 });
 
-app.get('/members/all', (req, res) => {
-    res.send((data));
+
+app.get('/members/', (req, res) => {
+    if (req.query.fields){
+        new_data_list = []
+        for (member of data){
+            var new_datum = {}
+            for (field_val of req.query.fields.split(",")){
+                if (!(field_val in member)) {
+                    res.status(400).send("Invalid field: "+field_val)
+                    return 
+                }
+                new_datum[field_val] = member[field_val]
+            }
+            new_data_list.push(new_datum)
+        }
+    }
+    else new_data_list = data
+    res.send(new_data_list);
 });
 
-app.get('/members/all/:attribute', (req, res) => {
+app.get('/members/count/:attribute', (req, res) => {
     attribute_count_list = []
-
     for (member of data){
         if (req.params.attribute == "employer"){
             for (job of member.jobs){
@@ -28,6 +50,10 @@ app.get('/members/all/:attribute', (req, res) => {
             }
         }
         else{
+            if (!(req.params.attribute in member)){
+                res.status(400).send("Invalid attribute: "+req.params.attribute)
+                return
+            }        
             if (!attribute_count_list.find(val => val[req.params.attribute] === member[req.params.attribute])){
                 item = {}
                 item[req.params.attribute] = member[req.params.attribute]
@@ -39,6 +65,7 @@ app.get('/members/all/:attribute', (req, res) => {
     }
     res.send((attribute_count_list));
 });
+
 app.get('/members/match/', (req, res) => {
     var matching_members = []
     for (let member of data){
@@ -49,49 +76,172 @@ app.get('/members/match/', (req, res) => {
                     matching = false
                 }
             }
-            else if(member[attribute] !== req.query[attribute]){
+            else {
+                if (!(attribute in member)){
+                    res.status(400).send("Invalid attribute: "+attribute)
+                    return
+                }
+                if(member[attribute] !== req.query[attribute]){
                 matching = false
+                }
             }
+
         }
-        if (matching) matching_members.push(member.name)
+        if (matching) matching_members.push({name:member.name , id: member.id})
     }
     res.send((matching_members));
 });
 
-app.get('/members/:name', (req, res) => {
-    res.send(data.find(({ name }) => name === req.params.name));
+app.get('/members/:id', (req, res) => {
+    member = data.find(member => member.id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+
+    if (req.query.fields){
+        shortened_member = {}
+        for (field of req.query.fields.split(",")){
+            if (!(field_val in member)) {
+                res.status(400).send("Invalid field: "+field_val)
+                return 
+            }
+            shortened_member[field] = member[field]
+        }
+    }
+    else shortened_member = member
+
+    res.send(shortened_member);
 });
 
-app.get('/members/:name/:attribute', (req, res) => {
-    res.send(data.find(({ name }) => name === req.params.name)[req.params.attribute]);
+
+app.get('/members/:id/jobs',(req, res) => {
+    member = data.find(({id}) => id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+    jobs = member.jobs
+    if (req.query.fields){
+        shortened_jobs = []
+        for(job of jobs){
+            short_job = {}
+            for (field of req.query.fields.split(",")){
+                if (!(field_val in job)) {
+                    res.status(400).send("Invalid field: "+field_val)
+                    return 
+                }
+                short_job[field] = job[field]
+            }
+            shortened_jobs.push(short_job)
+        }
+    }
+    else shortened_jobs = jobs
+    res.send(shortened_jobs);
 });
 
-app.get('/members/:name/jobs/:company_name', (req, res) => {
-    member = data.find(({ name }) => name === req.params.name)
-    res.send(member.jobs.find(({company_name}) => company_name === req.params.company_name));
+app.get('/members/:id/jobs/:job_id', (req, res) => {
+    member = data.find(({id}) => id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+
+    jobs = member.jobs
+    job = jobs.find(({job_id}) => job_id == req.params.job_id)
+    if (!job) res.status(404).send("Job id not found")
+
+    if (req.query.fields){
+        short_job = {}
+        for (field of req.query.fields.split(",")){
+            if (!(field_val in job)) {
+                res.status(400).send("Invalid field: "+field_val)
+                return 
+            }
+            short_job[field] = job[field]
+        }
+    }
+    else short_job = job
+    res.send(short_job);
 });
 
 app.post('/members', (req, res) => {
-    new_member = req.body
+    const new_member = req.body
+    console.log("here")
+    result = member_schema.validate(new_member, { presence: 'required'})
+    const { value, error } = result; 
+    
+    if(error) {
+        res.status(400).json(error)
+        return
+    }
+    new_id = data[data.length-1].id +1
+    new_member.id = new_id
     data.push(new_member)
-    res.json({message: "DALI member added"})
+    res.json({message: "DALI member added", id:(new_id)})
 });
 
-app.post('/members/:name/jobs', (req, res) => {
+app.use(function(err, req, res, next){
+    res.status(400).json(err);
+  });
+  
+app.post('/members/:id/jobs', (req, res) => {
+    member = data.find(({id}) => id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+
     job = req.body
-    job_list = data.find(({ name }) => name === req.params.name)["jobs"]
+    result = job_schema.validate(job,{ presence: 'required'})
+    const { value, error } = result; 
+    if(error) {
+        res.status(400).json(error)
+        return
+    }
+    job_list =  member.jobs
+    new_id = job_list[job_list.length-1].job_id +1
+    job.job_id = new_id
     job_list.push(job)
-    res.json({message:"Job added"})
+    res.json({message:"Job added Sucessfully", job_id:new_id})
 });
     
 
-app.put('/', (req, res) => {
+app.delete('/members/:id/jobs/:job_id', (req, res) => {
+    member = data.find(({id}) => id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
 
-return res.send('Received a PUT HTTP method');
+    member = data.find(member => member.id == req.params.id)
+    member.jobs = member.jobs.filter(job => job.job_id != req.params.job_id)
+return res.send('Job Deleted with id '+req.params.job_id);
 });
 
-app.delete('/', (req, res) => {
-return res.send('Received a DELETE HTTP method');
+app.patch('/members/:id' , (req,res) => {
+    member = data.find(member => member.id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+    var member_update = req.body
+
+    result = member_schema.validate(member_update,{ presence: 'optional' })
+    const { value, error } = result; 
+    if(error) {
+        res.status(400).json(error)
+        return
+    }
+    for (attribute in member_update) member[attribute] = member_update[attribute]
+return res.send('Member updated with id'+req.params.id);
+});
+
+app.patch('/members/:id/jobs/:job_id' , (req,res) => {
+    member = data.find(member => member.id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+
+    var job_upate = req.body
+    result = job_schema.validate(job_schema,{ presence: 'optional' })
+
+    jobs = member.jobs
+    job = jobs.find(job => job.job_id == req.params.job_id)
+    if (!job) res.status(404).send("Job id not found")
+    for (attribute in job_upate) job[attribute] = job_upate[attribute]
+    return res.send('Job updated with id'+req.params.job_id);
+});
+    
+app.delete('/members/:id/', (req, res) => {
+    member = data.find(member => member.id == req.params.id)
+    if (!member) res.status(404).send("User id not found")
+
+    job = jobs.find(job => job.job_id == req.params.job_id)
+    if (!job) res.status(404).send("Job id not found")
+
+    data = data.filter(member => member.id != req.params.id)
+return res.send('Member Deleted with id'+req.params.id);
 });
 
 app.listen(port, () => console.log(`API Server listening on port ${port}!`))
